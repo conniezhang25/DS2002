368845454f0b8cd09b8d2fe8b3d586615c9329b6
conniezhang25 <36180578+conniezhang25@users.noreply.github.com>
2023-02-09 17:02:32 +0000

    lab2a finished

--- DS-2002-002-main/01-SQL/Labs/Lab_02a_Create_Datawarehouse.sql
+++ DS-2002-002-main/01-SQL/Labs/Lab_02a_Create_Datawarehouse.sql
@@ -102,11 +102,81 @@ CREATE TABLE `dim_suppliers` (
   KEY `last_name` (`last_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
 
-
 -- ----------------------------------------------------------------------
 -- TODO: JOIN the orders, order_details, order_details_status and 
 --       orders_status tables to create a new Fact Table in Northwind_DW.
 -- To keep things simple, don't include purchase order or inventory info
 -- ----------------------------------------------------------------------
 # DROP TABLE `fact_orders`;
-CREATE TABLE `fact_orders`;
+#CREATE TABLE `fact_orders`;
+#status id joins with order status, replace status id to status name
+
+CREATE TABLE `fact_orders` (
+  `fact_order_key` int NOT NULL AUTO_INCREMENT,
+  `order_id` int NOT NULL,
+  `employee_id` int DEFAULT NULL,
+  `customer_id` int DEFAULT NULL,
+  `product_id` int DEFAULT NULL,
+  `shipper_id` int DEFAULT NULL,
+  `ship_name` varchar(50) DEFAULT NULL,
+  `ship_address` longtext,
+  `ship_city` varchar(50) DEFAULT NULL,
+  `ship_state_province` varchar(50) DEFAULT NULL,
+  `ship_zip_postal_code` varchar(15) DEFAULT NULL,
+  `ship_country_region` varchar(50) DEFAULT NULL,
+  `quantity` decimal(18,4) NOT NULL DEFAULT '0.0000',
+  `order_date` datetime DEFAULT NULL,
+  `shipped_date` datetime DEFAULT NULL,
+  `unit_price` decimal(19,4) DEFAULT '0.0000',
+  `discount` double NOT NULL DEFAULT '0',
+  `shipping_fee` decimal(19,4) DEFAULT '0.0000',
+  `payment_type` varchar(50) DEFAULT NULL,
+  `paid_date` datetime DEFAULT NULL,
+  `tax_rate` double DEFAULT '0',
+  `order_status` varchar(50) NOT NULL,
+  `order_details_status` varchar(50) NOT NULL,
+  
+  PRIMARY KEY (`fact_order_key`),
+  KEY `customer_id` (`customer_id`),
+  KEY `employee_id` (`employee_id`),
+  KEY `order_id` (`order_id`),
+  KEY `order_details_id` (`order_details_id`),
+  KEY `shipper_id` (`shipper_id`)
+  #KEY `fk_orders_status` (`order_status`)
+ # CONSTRAINT `fk_orders_customers` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`),
+ # CONSTRAINT `fk_orders_employees1` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`),
+ # CONSTRAINT `fk_orders_status1` FOREIGN KEY (`status_id`) REFERENCES `orders_status` (`id`),
+ # CONSTRAINT `fk_orders_tax_status1` FOREIGN KEY (`tax_status_id`) REFERENCES `orders_tax_status` (`id`),
+ # CONSTRAINT `fk_orders_shippers1` FOREIGN KEY (`shipper_id`) REFERENCES `shippers` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `customers` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `company` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `email_address` varchar(50) DEFAULT NULL,
+  `job_title` varchar(50) DEFAULT NULL,
+  `business_phone` varchar(25) DEFAULT NULL,
+  `home_phone` varchar(25) DEFAULT NULL,
+  `mobile_phone` varchar(25) DEFAULT NULL,
+  `fax_number` varchar(25) DEFAULT NULL,
+  `address` longtext,
+  `city` varchar(50) DEFAULT NULL,
+  `state_province` varchar(50) DEFAULT NULL,
+  `zip_postal_code` varchar(15) DEFAULT NULL,
+  `country_region` varchar(50) DEFAULT NULL,
+  `web_page` longtext,
+  `notes` longtext,
+  `attachments` longblob,
+  PRIMARY KEY (`id`),
+  KEY `city` (`city`),
+  KEY `company` (`company`),
+  KEY `first_name` (`first_name`),
+  KEY `last_name` (`last_name`),
+  KEY `zip_postal_code` (`zip_postal_code`),
+  KEY `state_province` (`state_province`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb3;
+
+#join order and order status, and order details and order status details, then join orders and order details
+
